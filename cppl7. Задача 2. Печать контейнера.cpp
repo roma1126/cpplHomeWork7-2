6a32b60b3b1eb8e7c54be2fe94ebfa58821ead13
Roman <romka200211@gmail.com>
2023-08-14 16:49:44 +0300

    first commit

---
+++ cppl7. Задача 2. Печать контейнера.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+#include <algorithm>
+
+template <class T>
+void print_container(T test) 
+{
+    for (const auto& elements : test)
+        std::cout << elements << " ";
+    std::cout << std::endl;
+}
+
+int main() 
+{
+
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+
+}
